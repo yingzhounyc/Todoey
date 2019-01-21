c8ac7df125cbc34ac9d66bf6c48f0b3630565918
Ying <ying.zhou@qc.cuny.edu>
2019-01-21 08:38:03 -0500

    Add a message of "no categories added" if no cell is loaded.

--- Todoey/Controllers/CategoryTableViewController.swift
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -13,28 +13,28 @@ class CategoryTableViewController: UITableViewController {
     
     let realm = try! Realm()
     
-    var categories: Results<Category>!
+    var categories: Results<Category>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-     loadCategories()
+ //    loadCategories()
     }
     
     //Mark: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        return categories?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        let category = categories[indexPath.row]
+ //       let category = categories[indexPath.row]
         
-       cell.textLabel?.text = category.name
+       cell.textLabel?.text = categories?[indexPath.row].name ?? "No categories added yet"
         
         return cell
     }
@@ -101,7 +101,7 @@ class CategoryTableViewController: UITableViewController {
         let destinationVC = segue.destination as! TodoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categories[indexPath.row]
+            destinationVC.selectedCategory = categories?[indexPath.row]
         }
     }
 }
